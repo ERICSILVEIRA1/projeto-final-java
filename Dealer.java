e27056182bbd5d8f9f727b50e11dc80dd239da56
AdhanCarvalho <carvalhoadhan16@gmail.com>
2024-06-26 22:10:55 -0300

    atualizei a classe dealer, talvez tenha que modificar

--- Dealer.java
+++ Dealer.java
@@ -3,25 +3,42 @@ public class Dealer {
     private Player player;
 
     public Dealer(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
         this.player = new Player(name);
         this.hand = new Hand();
     }
 
     public void setPlayer(Player player) {
+        if (player == null) {
+            throw new IllegalArgumentException("Player cannot be null");
+        }
         this.player = player;
     }
 
     public void drawCard(Deck deck) {
+        if (deck == null) {
+            throw new IllegalArgumentException("Deck cannot be null");
+        }
         Card card = deck.drawCard();
         hand.addCard(card);
     }
 
     public void printFirstHand() {
-        System.out.println(player.getName() + "'s first hand: " + hand.getCards().get(0));
+        if (hand.getCards().isEmpty()) {
+            System.out.println(player.getName() + " has no cards in hand.");
+        } else {
+            System.out.println(player.getName() + "'s first hand: " + hand.getCards().get(0));
+        }
     }
 
     public void printHand() {
-        System.out.println(player.getName() + "'s hand: " + hand);
+        if (hand.getCards().isEmpty()) {
+            System.out.println(player.getName() + " has no cards in hand.");
+        } else {
+            System.out.println(player.getName() + "'s hand: " + hand);
+        }
     }
 
     public Hand getHand() {
